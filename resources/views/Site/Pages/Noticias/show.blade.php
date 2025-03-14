e2f6daae669d4385a92e2af03e7f8b150d458ee9
NicolauMiguel <olimposn6@gmail.com>
2025-03-14 12:43:13 +0100

    Last Update

--- resources/views/Site/Pages/Noticias/show.blade.php
+++ resources/views/Site/Pages/Noticias/show.blade.php
@@ -1,5 +1,5 @@
 @extends('Site/layouts/page')
-@section('title')Mig @endsection
+@section('title') Nicolaudio @endsection
 @section('conteudo')
  <h1>PUDIM</h1>
 @endsection
---
+++ resources/views/Site/layouts/footer.blade.php
@@ -0,0 +1,36 @@
+
+
+
+
+<footer class="footer">
+  <div class="d-sm-flex justify-content-center justify-content-sm-between">
+    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2023. Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash. All rights reserved.</span>
+    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="ti-heart text-danger ms-1"></i></span>
+  </div>
+</footer>
+    
+</div>
+    <!-- plugins:js -->
+
+    <script src="{{asset('assets/vendors/js/vendor.bundle.base.js')}}"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="{{asset('assets/vendors/chart.js/chart.umd.js')}}"></script>
+    <script src="{{asset('assets/vendors/datatables.net/jquery.dataTables.js')}}"></script>
+    <!-- <script src="{{asset('assets/vendors/datatables.net-bs4/dataTables.bootstrap4.js')}}"></script> -->
+    <script src="{{asset('assets/vendors/datatables.net-bs5/dataTables.bootstrap5.js')}}"></script>
+    <script src="{{asset('assets/js/dataTables.select.min.js')}}"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="{{asset('assets/js/off-canvas.js')}}"></script>
+    <script src="{{asset('assets/js/template.js')}}"></script>
+    <script src="{{asset('assets/js/settings.js')}}"></script>
+    <script src="{{asset('assets/js/todolist.js')}}"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page-->
+    <script src="{{asset('assets/js/jquery.cookie.js')}}" type="text/javascript"></script>
+    <script src="{{asset('assets/js/dashboard.js')}}"></script>
+    <!-- <script src="assets/js/Chart.roundedBarCharts.js"></script> -->
+    <!-- End custom js for this page-->
+  </body>
+</html>
